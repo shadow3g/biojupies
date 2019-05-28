b7786e8f1a35a666653102df8fb5ab83bf865061
denis-torre <denis.torre@mssm.edu>
2019-05-28 18:25:01 -0400

    Chrome error catch

--- website/app/app.py
+++ website/app/app.py
@@ -1295,9 +1295,12 @@ def notebook_generator_server_api(path):
 	if path in ['api/generate', 'api/help']:
 		j = request.json
 		r = requests.post('{NOTEBOOK_GENERATOR_SERVER_BASE_URL}/{path}'.format(**os.environ, **locals()), json=j)
-		result = jsonify(r.json())
-		result.status_code = r.status_code 
-		# capture errors for server and extension
+		print(r.text)
+		if '<br><br>' not in r.text:
+			result = jsonify(r.json())
+			result.status_code = r.status_code 
+		else:
+			result = r.text
 		return result
 	elif path == 'api/version':
 		r = requests.post('{NOTEBOOK_GENERATOR_SERVER_BASE_URL}/{path}'.format(**os.environ, **locals()))
